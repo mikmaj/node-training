a06eb3c503fff91a80300613a0829f3388987842
Mikko Maja <mikmaj88@gmail.com>
2019-01-29 18:09:58 +0200

    Add mods for heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
+
 let app = express()
 
 // Let hbs know we want to use partials for repeated html constructs
@@ -54,6 +56,6 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server up on port 3000')
+app.listen(port, () => {
+    console.log(`Server up on port ${port}`)
 })
